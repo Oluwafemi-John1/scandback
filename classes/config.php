0584b10e8b27ccfcf495f09537974ab907c63caa
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2023-04-04 16:10:47 +0100

    make it better

--- classes/config.php
+++ classes/config.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *"); 
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding, Authorization, X-Requested-With");
 header("Content-type: application/json; charset=UTF-8");
@@ -8,24 +8,30 @@ header("Content-type: application/json; charset=UTF-8");
 require "ConfigAbstract.php";
 class Config
 {
-    protected $localhost = 'us-cdbr-east-06.net';
-    // protected $username = 'root'; 
-    // protected $dbName = 'scandiweb';
-    // protected $password = '';
-    protected $username = 'b5e90a7634be98';
-    protected $dbName = 'config';
-    protected $password = 'd48c8ea9'; 
+    public $cleardb_url;
+    public $cleardb_password;
+    public $cleardb_server;
+    public $cleardb_db;
+    public $cleardb_username;
+    public $active_group = 'default';
+    public $query_builder = TRUE;
     public $connectdb = "";
     public $res = [];
     public function __construct()
     {
-        $config = new DatabaseConfig($this->localhost, $this->username,$this->password, $this->dbName);
+        $this->cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $this->cleardb_password = isset($this->cleardb_url["pass"]) ? $this->cleardb_url["pass"] : '';
+        $this->cleardb_server = isset($this->cleardb_url["host"]) ? $this->cleardb_url["host"] : '';
+        $this->cleardb_db = isset($this->cleardb_url["path"]) ? substr($this->cleardb_url["path"], 1) : '';
+        $this->cleardb_username = isset($this->cleardb_url["user"]) ? $this->cleardb_url["user"] : '';
+
+        $config = new DatabaseConfig($this->cleardb_server, $this->cleardb_username, $this->cleardb_password, $this->cleardb_db);
         $connectionObject = new DatabaseConnection($config);
         $this->connectdb = $connectionObject->getConnection();
+         
+        $table_name = "Products";
 
-            $table_name = "Products";
-
-            $query = "CREATE TABLE IF NOT EXISTS $table_name (
+        $query = "CREATE TABLE IF NOT EXISTS $table_name (
             ProductID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
             sku VARCHAR(255) NOT NULL UNIQUE,
             name VARCHAR(255) NOT NULL,
@@ -38,12 +44,11 @@ class Config
             length int(20)
         )";
 
-            $statement = $this->connectdb->query($query);
+        $statement = $this->connectdb->query($query);
 
-            if (!$statement) {
-                echo "Error creating table: " . $this->connectdb->error;
-            }
-        
+        if (!$statement) {
+            echo "Error creating table: " . $this->connectdb->error;
+        }
     }
 
     //  public function create($query, $binder)
@@ -68,19 +73,19 @@ class Config
     //     return $this->res;
     // }
 
-    public function create($query,$binder){
-        $statement = $this ->connectdb->prepare($query);
-         $statement->bind_param(...$binder);
-        if($statement->execute()){
-          $this->res['success']= true;
-          $this->res['message'] = "Product created successfully";
+    public function create($query, $binder)
+    {
+        $statement = $this->connectdb->prepare($query);
+        $statement->bind_param(...$binder);
+        if ($statement->execute()) {
+            $this->res['success'] = true;
+            $this->res['message'] = "Product created successfully";
+        } else {
+            $this->res['success'] = false;
+            $this->res['message'] = "Product can not be created successfully";
         }
-        else{
-          $this ->res['success'] = false;
-          $this->res['message'] = "Product can not be created successfully";
-      }
-       return $this->res;
-      }
+        return $this->res;
+    }
     public function read($query, $binder)
     {
         $statement = $this->connectdb->prepare($query);
@@ -92,7 +97,6 @@ class Config
             $fetch = $statement->get_result();
             $this->res['success'] = true;
             $this->res['result'] = mysqli_fetch_all($fetch, MYSQLI_ASSOC);
-
         } else {
             $this->res['success'] = false;
         }
@@ -119,5 +123,4 @@ class Config
         }
         return $this->res;
     }
-
 }
