d48dec7206ad78f622c154320609f04c5b0d4cd7
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2023-04-06 15:03:48 +0100

    make it simple

--- createProduct.php
+++ createProduct.php
@@ -2,7 +2,7 @@
 
 require "classes/Product.php";
 $_POST = json_decode(file_get_contents("php://input"),true);
-$sku = $_POST['sku'];
+$sku = $_POST->sku;
 $name = $_POST['name'];
 $price = $_POST['price'];
 $productType = $_POST['productType'];
@@ -15,3 +15,4 @@ $length = $_POST['length'];
 $product = new Product($sku, $name, $price, $productType, $size, $weight, $height, $width, $length);
 $saved = $product->save();
 echo json_encode($saved);
+?>
