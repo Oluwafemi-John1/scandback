01881f92b529dc3fafc1638d052a9f0c41063a8a
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2023-04-12 14:47:19 +0100

    Update createProduct.php

--- createProduct.php
+++ createProduct.php
@@ -1,17 +1,29 @@
 <?php
 
-require "classes/Product.php";
-$_POST = json_decode(file_get_contents("php://input"),true);
-$sku = $_POST['sku'];
-$name = $_POST['name'];
-$price = $_POST['price'];
-$productType = $_POST['productType'];
-$size = $_POST['size'];
-$weight = $_POST['weight'];
-$height = $_POST['height'];
-$width = $_POST['width'];
-$length = $_POST['length'];
+require_once "classes/Product.php";
 
+// Get the JSON data from the request body
+$data = json_decode(file_get_contents("php://input"), true);
+
+// Sanitize the input data
+$sku = filter_var($data['sku'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$name = filter_var($data['name'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$price = filter_var($data['price'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+$productType = filter_var($data['productType'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$size = filter_var($data['size'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+$weight = filter_var($data['weight'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+$height = filter_var($data['height'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+$width = filter_var($data['width'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+$length = filter_var($data['length'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+
+// Create a new product object
 $product = new Product($sku, $name, $price, $productType);
+
+// Save the product and get the result
 $saved = $product->save($sku, $name, $price, $productType, $size, $weight, $height, $width, $length);
-echo json_encode($_POST);
+
+// Send the response as JSON
+header("Content-Type: application/json");
+echo json_encode($saved);
+
+?>
